b68e52e296789a925dce0a17ba03df7eae4f86cc
Karan Sah <iit2019046@iiita.ac.in>
2025-03-24 07:08:22 +0530

    fix nav stack bug

--- MedBook/Classes/Login/LoginView.swift
+++ MedBook/Classes/Login/LoginView.swift
@@ -22,84 +22,86 @@ struct LoginView: View {
     @State private var navigateToHomePage = false
     
     var body: some View {
-        VStack {
-            
-            Text("log in to continue")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(
-                    Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
-                )
-                .padding(.leading, 20)
-                .font(.title)
-            
-            VStack(spacing: 20) {
-                EmailEntryView(email: $login.email, isValidEmail: $isValidEmail)
-                    .onChange(of: isValidEmail) { _, _ in allFieldsVaild() }
-                PasswordEntryView(password: $login.password, isValidPassword: $isValidPassword, validationRequired: false)
-                    .onChange(of: login.password) { _, _ in allFieldsVaild() }
-            }
-            
-            Spacer()
-            
-            Button(action:{
-                Task {
-                    await viewModel.checkValidCreds(login)
-                    if viewModel.validCreds {
-                        sessionManager.login()
-                        dismiss()
-                        navigateToHomePage = true
-                    } else {
-                        showAlert = true
-                        alertMessage = "Credentials are not Valid \n Please enter correct details"
-                    }
-                }
-            })
-            {
-                HStack {
-                    Text("Login")
-                        .font(.headline)
-                        .foregroundColor(
-                            allFieldsValid ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) : Color(#colorLiteral(red: 0.6533104777, green: 0.6533104777, blue: 0.6533104777, alpha: 1))
-                        )
-                    
-                    Image(systemName: "arrow.right")
-                        .resizable()
-                        .frame(width: 20, height: 15)
-                        .foregroundColor(
-                            allFieldsValid ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) : Color(#colorLiteral(red: 0.6533104777, green: 0.6533104777, blue: 0.6533104777, alpha: 1))
-                        )
-                }
-                .padding()
-                .cornerRadius(10)
-            }
-            .disabled(!allFieldsValid)
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(
-                        allFieldsValid ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) : Color(#colorLiteral(red: 0.6533104777, green: 0.6533104777, blue: 0.6533104777, alpha: 1)),
-                        lineWidth: 0.5
+        NavigationStack {
+            VStack {
+                
+                Text("log in to continue")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(
+                        Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
                     )
-                    .frame(width: 0.5*UIScreen.main.bounds.width, height: 50)
-            )
-            .frame(width: 0.5*UIScreen.main.bounds.width, height: 50)
-            .font(.title3)
-            .padding(.top, 30)
-            .padding(.bottom, 50)
-            
-            if viewModel.processing {
-                ProgressView()
+                    .padding(.leading, 20)
+                    .font(.title)
+                
+                VStack(spacing: 20) {
+                    EmailEntryView(email: $login.email, isValidEmail: $isValidEmail)
+                        .onChange(of: isValidEmail) { _, _ in allFieldsVaild() }
+                    PasswordEntryView(password: $login.password, isValidPassword: $isValidPassword, validationRequired: false)
+                        .onChange(of: login.password) { _, _ in allFieldsVaild() }
+                }
+                
+                Spacer()
+                
+                Button(action:{
+                    Task {
+                        await viewModel.checkValidCreds(login)
+                        if viewModel.validCreds {
+                            sessionManager.login()
+                            dismiss()
+                            navigateToHomePage = true
+                        } else {
+                            showAlert = true
+                            alertMessage = "Credentials are not Valid \n Please enter correct details"
+                        }
+                    }
+                })
+                {
+                    HStack {
+                        Text("Login")
+                            .font(.headline)
+                            .foregroundColor(
+                                allFieldsValid ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) : Color(#colorLiteral(red: 0.6533104777, green: 0.6533104777, blue: 0.6533104777, alpha: 1))
+                            )
+                        
+                        Image(systemName: "arrow.right")
+                            .resizable()
+                            .frame(width: 20, height: 15)
+                            .foregroundColor(
+                                allFieldsValid ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) : Color(#colorLiteral(red: 0.6533104777, green: 0.6533104777, blue: 0.6533104777, alpha: 1))
+                            )
+                    }
+                    .padding()
+                    .cornerRadius(10)
+                }
+                .disabled(!allFieldsValid)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(
+                            allFieldsValid ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) : Color(#colorLiteral(red: 0.6533104777, green: 0.6533104777, blue: 0.6533104777, alpha: 1)),
+                            lineWidth: 0.5
+                        )
+                        .frame(width: 0.5*UIScreen.main.bounds.width, height: 50)
+                )
+                .frame(width: 0.5*UIScreen.main.bounds.width, height: 50)
+                .font(.title3)
+                .padding(.top, 30)
+                .padding(.bottom, 50)
+                
+                if viewModel.processing {
+                    ProgressView()
+                }
             }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Login"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+            }
+            .navigationDestination(isPresented: $navigateToHomePage) {
+                HomeView()
+            }
+            .navigationTitle("Welcome")
+            .background(
+                linearGradient.ignoresSafeArea()
+            )
         }
-        .alert(isPresented: $showAlert) {
-            Alert(title: Text("Login"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-        }
-        .navigationDestination(isPresented: $navigateToHomePage) {
-            HomeView()
-        }
-        .navigationTitle("Welcome")
-        .background(
-            linearGradient.ignoresSafeArea()
-        )
     }
     
     private func allFieldsVaild()  {
--- MedBook/Classes/SignUp/SignUpView.swift
+++ MedBook/Classes/SignUp/SignUpView.swift
@@ -24,87 +24,89 @@ struct SignUpView: View {
     @State private var navigateToHomePage = false
     
     var body: some View {
-        VStack(spacing: 20) {
-            
-            Text("sign up to continue")
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .foregroundColor(
-                    Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
-                )
-                .padding(.leading, 20)
-                .font(.title)
-            
-            EmailEntryView(email: $signUp.email, isValidEmail: $isValidEmail)
-                .onChange(of: isValidEmail) { _, _ in allFieldsVaild() }
-            
-            PasswordEntryView(password: $signUp.password, isValidPassword: $isValidPassword, validationRequired: true)
-                .onChange(of: isValidPassword) { _, _ in allFieldsVaild() }
-            
-            CountryView(country: $signUp.country, isValidCountry: $isValidCountry)
-                .onChange(of: isValidCountry) { _, _ in allFieldsVaild() }
-            
-            Spacer()
-            
-            Button(action: {
-                viewModel.signUpCompletion = { success, error in
-                    if success {
-                        sessionManager.login()
-                        navigateToHomePage = true
-                    } else {
-                        showAlert = true
-                        alertMessage = "Signup failed!\n\(error?.localizedDescription ?? "Unknown error")"
+        NavigationStack {
+            VStack(spacing: 20) {
+                
+                Text("sign up to continue")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .foregroundColor(
+                        Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1))
+                    )
+                    .padding(.leading, 20)
+                    .font(.title)
+                
+                EmailEntryView(email: $signUp.email, isValidEmail: $isValidEmail)
+                    .onChange(of: isValidEmail) { _, _ in allFieldsVaild() }
+                
+                PasswordEntryView(password: $signUp.password, isValidPassword: $isValidPassword, validationRequired: true)
+                    .onChange(of: isValidPassword) { _, _ in allFieldsVaild() }
+                
+                CountryView(country: $signUp.country, isValidCountry: $isValidCountry)
+                    .onChange(of: isValidCountry) { _, _ in allFieldsVaild() }
+                
+                Spacer()
+                
+                Button(action: {
+                    viewModel.signUpCompletion = { success, error in
+                        if success {
+                            sessionManager.login()
+                            navigateToHomePage = true
+                        } else {
+                            showAlert = true
+                            alertMessage = "Signup failed!\n\(error?.localizedDescription ?? "Unknown error")"
+                        }
                     }
+                    Task {
+                        await viewModel.createUserAndLogin(User(name: signUp.name, email: signUp.email, password: signUp.password, country: signUp.country))
+                    }
+                }) {
+                    HStack {
+                        Text("Let's go")
+                            .font(.headline)
+                        
+                        Image(systemName: "arrow.right")
+                            .resizable()
+                            .frame(width: 20, height: 15)
+                    }
+                    .padding()
+                    .cornerRadius(10)
                 }
-                Task {
-                    await viewModel.createUserAndLogin(User(name: signUp.name, email: signUp.email, password: signUp.password, country: signUp.country))
+                .disabled(!allFieldsValid)
+                .foregroundColor(
+                    allFieldsValid ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) : Color(#colorLiteral(red: 0.6533104777, green: 0.6533104777, blue: 0.6533104777, alpha: 1))
+                )
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(
+                            allFieldsValid ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) : Color(#colorLiteral(red: 0.6533104777, green: 0.6533104777, blue: 0.6533104777, alpha: 1)),
+                            lineWidth: 0.5)
+                        .frame(width: 0.5*UIScreen.main.bounds.width, height: 50)
+                )
+                .frame(width: 0.8*UIScreen.main.bounds.width, height: 50)
+                .font(.title3)
+                .padding(.top, 20)
+                .padding(.bottom, 50)
+                
+                if viewModel.processing {
+                    ProgressView()
                 }
-            }) {
-                HStack {
-                    Text("Let's go")
-                        .font(.headline)
-                    
-                    Image(systemName: "arrow.right")
-                        .resizable()
-                        .frame(width: 20, height: 15)
-                }
-                .padding()
-                .cornerRadius(10)
             }
-            .disabled(!allFieldsValid)
-            .foregroundColor(
-                allFieldsValid ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) : Color(#colorLiteral(red: 0.6533104777, green: 0.6533104777, blue: 0.6533104777, alpha: 1))
-            )
-            .overlay(
-                RoundedRectangle(cornerRadius: 10)
-                    .stroke(
-                        allFieldsValid ? Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)) : Color(#colorLiteral(red: 0.6533104777, green: 0.6533104777, blue: 0.6533104777, alpha: 1)),
-                        lineWidth: 0.5)
-                    .frame(width: 0.5*UIScreen.main.bounds.width, height: 50)
-            )
-            .frame(width: 0.8*UIScreen.main.bounds.width, height: 50)
-            .font(.title3)
-            .padding(.top, 20)
-            .padding(.bottom, 50)
-            
-            if viewModel.processing {
-                ProgressView()
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Sign Up"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
             }
+            .onChange(of: navigateToHomePage, { oldValue, newValue in
+                guard newValue else { return }
+                dismiss()
+            })
+            .navigationDestination(isPresented: $navigateToHomePage) {
+                HomeView()
+            }
+            .frame(maxHeight: .infinity)
+            .navigationTitle("Welcome")
+            .background(
+                linearGradient.ignoresSafeArea()
+            )
         }
-        .alert(isPresented: $showAlert) {
-            Alert(title: Text("Sign Up"), message: Text(alertMessage), dismissButton: .default(Text("OK")))
-        }
-        .onChange(of: navigateToHomePage, { oldValue, newValue in
-            guard newValue else { return }
-            dismiss()
-        })
-        .navigationDestination(isPresented: $navigateToHomePage) {
-            HomeView()
-        }
-        .frame(maxHeight: .infinity)
-        .navigationTitle("Welcome")
-        .background(
-            linearGradient.ignoresSafeArea()
-        )
     }
     
     private func allFieldsVaild() {
